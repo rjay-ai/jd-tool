b57bc2d6ab07607bfcfe879b6b6bb90f8710aa32
rjay-ai <rjsubscriptionz@gmail.com>
2024-11-20 14:15:30 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-```javascript
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  env: {
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_DRIVE_FOLDER_ID: process.env.GOOGLE_DRIVE_FOLDER_ID,
+  }
+};
 
-module.exports = nextConfig
-```
+module.exports = nextConfig;
